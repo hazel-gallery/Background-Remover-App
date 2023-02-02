e6eab29752a55aee5486b9f395cd517ff9d1b5bd
Johnny Chan <johnnychants@gmail.com>
2023-02-01 20:03:51 -0700

    Modified shell script to manage.sh
    1. Removed the system update part because it should be taken care at the time instance creates
    2. Separate app start up and down down.

--- manage.sh
+++ manage.sh
@@ -3,12 +3,6 @@
 if [[ "$1" == "env" ]]
 then
 
-    echo "Updating system ......"
-    sudo yum -y update
-
-    echo "Installing git ......"
-    sudo yum -y install git
-
     echo "Downloading miniconda ......"
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
@@ -18,11 +12,17 @@ then
     echo "Install python package ......"
     miniconda3/bin/pip install -r demo_app/requirements.txt
 
-elif [[ "$1" == "app" ]] 
+elif [[ "$1" == "appup" ]] 
 then
 
-    echo "Starting app ......"
+    echo "Starting up app ......"
     nohup miniconda3/bin/streamlit run demo_app/app.py > output.txt &
+
+elif [[ "$1" == "appdown" ]] 
+then
+
+    echo "Shutting down app ......"
+    sudo kill -9 $(sudo lsof -t -i:8501)
 
 fi
 

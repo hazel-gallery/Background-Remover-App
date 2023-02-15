fec9d8f35436a2d11dcdefc69d43e746d04586fe
Johnny Chan <johnnychants@gmail.com>
2023-02-15 00:46:52 -0700

    Containerized app
    1. Added Dockerfile
    2. Created temp and outputs folder is not existed

--- manage.sh
+++ manage.sh
@@ -4,10 +4,10 @@ if [[ "$1" == "env" ]]
 then
 
     echo "Downloading miniconda ......"
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    wget https://repo.continuum.io/miniconda/Miniconda3-py39_23.1.0-1-Linux-x86_64.sh
 
     echo "Installing miniconda ......"
-    bash Miniconda3-latest-Linux-x86_64.sh -b
+    bash Miniconda3-py39_23.1.0-1-Linux-x86_64.sh
 
     echo "Install python package ......"
     miniconda3/bin/pip install -r demo_app/requirements.txt

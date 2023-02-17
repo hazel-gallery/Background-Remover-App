41ffb099c4e852c5861478c7b428a20e4cde8084
Johnny Chan <johnnychants@gmail.com>
2023-02-16 17:52:27 -0700

    Added Intro
    1. Removed the welcome page
    2. Added intro to the app and example image

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ from PIL import Image
 import shutil
 
 
-app_mode = st.sidebar.selectbox(
-    "Choose the app mode",
-    ["Welcome", "Backgournd Remover"]
-)
-
-
 def bgr(img_path, output_path):
     '''background remove'''
     res = subprocess.run(
@@ -32,7 +26,6 @@ def format_file_name(file_name):
     return fname2 + '.' + ftype
     
 
-
 def save_buffered(buffered_file,buffered_folder):
     # save the streamlit buffered files to temp folder
     if os.path.isdir(buffered_folder) == False:
@@ -40,52 +33,58 @@ def save_buffered(buffered_file,buffered_folder):
     with open(os.path.join(buffered_folder,buffered_file.name),"wb") as f:
         f.write(buffered_file.getbuffer())
 
-if app_mode == "Welcome":
-    st.write('Welcome')
 
-elif app_mode == "Backgournd Remover":
-    if os.path.isdir('temp'):
-        pass
-    else:
-        os.mkdir('temp')
-    if os.path.isdir('outputs'):
-        pass
-    else:
-        os.mkdir('outputs')
+if os.path.isdir('temp'):
+    pass
+else:
+    os.mkdir('temp')
+if os.path.isdir('outputs'):
+    pass
+else:
+    os.mkdir('outputs')
 
-    st.title("Background Remover")
-    st.markdown('''
-        Intro here
-    ''')
-    files = st.sidebar.file_uploader(
-        label = "Upload images", 
-        accept_multiple_files = True
+st.title("Background Remover")
+st.markdown('''
+    This application utilizes deep learning to distinguish 
+    the object from the background of a photo. This is a 
+    standalone application that has no external dependencies, 
+    meaning all your photos are processed within the host 
+    machine (of your choice). It is private and secured. 
+''')
+example_img = Image.open('images/example_1.jpg')
+st.image(example_img)
+
+files = st.sidebar.file_uploader(
+    label = "Upload images", 
+    type = ['png', 'jpg'],
+    accept_multiple_files = True
+)
+btn = st.sidebar.button('Upload')
+if btn:
+    st.info('The processed images will be shown below')
+    purge_folder('temp/')
+    purge_folder('outputs/')
+    for fi in files:
+        fi.name = format_file_name(fi.name)
+        save_buffered(fi,'temp/')
+    files = os.listdir('temp/')
+    for fi in files:
+        src_path = 'temp/' + fi
+        fo = fi.split('.')[0] + '.png'
+        dest_path = 'outputs/' + fo 
+        bgr(src_path, dest_path)
+        image = Image.open(dest_path)
+        rotated_image = image.rotate(angle=0)
+        st.image(rotated_image)
+        rotated_image.save(dest_path)
+
+    cwd = os.getcwd()
+    shutil.make_archive(
+        base_name = cwd + '/outputs', 
+        format = 'zip',
+        root_dir = cwd,
+        base_dir = 'outputs',
     )
-    btn = st.sidebar.button('Upload')
-    if btn:
-        purge_folder('temp/')
-        purge_folder('outputs/')
-        for fi in files:
-            fi.name = format_file_name(fi.name)
-            save_buffered(fi,'temp/')
-        files = os.listdir('temp/')
-        for fi in files:
-            src_path = 'temp/' + fi
-            fo = fi.split('.')[0] + '.png'
-            dest_path = 'outputs/' + fo 
-            bgr(src_path, dest_path)
-            image = Image.open(dest_path)
-            rotated_image = image.rotate(angle=270)
-            st.image(rotated_image)
-            rotated_image.save(dest_path)
-
-        cwd = os.getcwd()
-        shutil.make_archive(
-            base_name = cwd + '/outputs', 
-            format = 'zip',
-            root_dir = cwd,
-            base_dir = 'outputs',
-        )
-        with open("outputs.zip", 'rb') as f:
-            st.sidebar.download_button('download as zip',f,file_name='bgr_images.zip')
-        os.remove('outputs.zip')
+    with open("outputs.zip", 'rb') as f:
+        st.sidebar.download_button('Download All',f,file_name='bgr_images.zip')
+    os.remove('outputs.zip')
